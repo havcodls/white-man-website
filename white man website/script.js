243d959a49f6eff8cd315d19e922fff9d4a5fee1
havcodls <160694522+havcodls@users.noreply.github.com>
2024-02-22 14:30:58 +0100

    script.js

--- white man website/script.js
+++ white man website/script.js
@@ -1,9 +1,27 @@
-const btn = document.querySelector('.scroll-btn');
+var btnn = document.querySelector(".btn-sect2");
+var container = document.querySelector(".container-container");
+var paragraphs = container.querySelectorAll("p");
 
-btn.addEventListener('click', () => {
+var originalHeight;
 
-    window.scrollTo({
-      top: window.innerHeight,      
-      behavior: "smooth"
-    })
-})
+var clics = 0;
+
+window.addEventListener('load', function() {
+  originalHeight = container.clientHeight;
+});
+
+btnn.addEventListener('click', () => {
+  if (window.innerWidth < 949) {
+    if (clics === 0) {
+      var totalHeight = 0;
+      paragraphs.forEach(function(paragraph) {
+        totalHeight += paragraph.offsetHeight + 60;
+      });
+      container.style.height = totalHeight + 'px';
+      clics++;
+    } else if (clics === 1) { 
+      container.style.height = originalHeight + 'px';
+      clics = 0;
+    }
+  }
+});
